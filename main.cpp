fd50f6018769531540e761677ad52b7c6ce315f4
oosakiharuki <k023g0035@g.neec.ac.jp>
2024-12-18 16:11:14 +0900

    06_02成功

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <string>
+#include <thread>
+#include <functional>
+#include <iostream>
 
-int main(void) {
+bool a = false;
+bool b = false;
+bool x = false;
+
+void thread1() {	
+	printf("thread1\n");
+	a = true;
+}
+
+void thread2() {
+	while (!a) {
+		if (a) {		
+			break;
+		}
+		else {
+			continue;
+		}
+	}	
+	printf("thread2\n");		
+	b = true;
+}
+
+void thread3() {
+	while (!b) {
+		if (b) {
+			break;
+		}
+		else {
+			continue;
+		}
+	}
+	printf("thread3\n");
+}
+
+
+int main() {
+
+
+	std::thread th1(thread1);		
+	std::thread th2(thread2);
+	std::thread th3(thread3);
+	
+	
+	th1.join();
+	th2.join();	
+	th3.join();
 
 
 	return 0;
